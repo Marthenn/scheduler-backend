c2dfc216c48c5a53df23e88bee4b47f735708c37
Marthenn <89301265+Marthenn@users.noreply.github.com>
2023-07-26 15:56:23 +0700

    feat: values checking before creating the new matakuliah object

--- internal/models/matakuliah.go
+++ internal/models/matakuliah.go
@@ -17,6 +17,17 @@ type MataKuliah struct {
 
 // NewMataKuliah creates a new MataKuliah object
 func NewMataKuliah(ID int, Nama string, SKS int, Jurusan string, SemesterMinimal int, PrediksiNilai string) {
+	// validate each values
+	if SKS < 1 {
+		panic("sks must be greater than 0")
+	}
+	if SemesterMinimal < 1 || SemesterMinimal > 8 {
+		panic("semester minimal must be between 1 and 8")
+	}
+	if !validateNilai(PrediksiNilai) {
+		panic("prediksi nilai must be one of A, AB, B, BC, C, D, E")
+	}
+
 	// create the matakuliah object
 	mk := MataKuliah{
 		ID:              ID,
@@ -27,4 +38,14 @@ func NewMataKuliah(ID int, Nama string, SKS int, Jurusan string, SemesterMinimal
 		PrediksiNilai:   PrediksiNilai,
 	}
 	MataKuliahSet.Insert(mk)
+}
+
+// validateNilai will checks if the PrediksiNilai is within the range of the allowed values
+func validateNilai(PrediksiNilai string) bool {
+	switch PrediksiNilai {
+	case "A", "AB", "B", "BC", "C", "D", "E":
+		return true
+	default:
+		return false
+	}
 }
