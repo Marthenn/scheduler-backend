9c92dff0e94d2dc9907d9649cde727047cd0d6bd
Marthenn <89301265+Marthenn@users.noreply.github.com>
2023-07-26 16:30:43 +0700

    feat: read of the CRUD of the database

---
+++ internal/database/read.go
@@ -0,0 +1,63 @@
+package database
+
+import (
+	"context"
+	"github.com/golang-collections/collections/set"
+	"scheduler-backend/internal/models"
+)
+
+// ReadJurusan reads all jurusan from the database
+func ReadJurusan() {
+	models.JurusanSet = set.New() // reset the set
+
+	conn := getDBConnection()
+	defer conn.Close(context.Background())
+
+	rows, err := conn.Query(context.Background(), "select * from jurusan")
+	if err != nil {
+		panic(err)
+	}
+
+	for rows.Next() {
+		var jurusan string
+		var fakultas string
+		err = rows.Scan(&jurusan, &fakultas)
+		if err != nil {
+			panic(err)
+		}
+		models.NewJurusan(jurusan, fakultas)
+	}
+}
+
+// ReadMataKuliah reads all mata kuliah from the database
+func ReadMataKuliah() {
+	models.MataKuliahSet = set.New() // reset the set
+
+	conn := getDBConnection()
+	defer conn.Close(context.Background())
+
+	rows, err := conn.Query(context.Background(), "select * from matakuliah")
+	if err != nil {
+		panic(err)
+	}
+
+	for rows.Next() {
+		var ID string
+		var Nama string
+		var SKS int
+		var Jurusan string
+		var SemesterMinimal int
+		var PrediksiNilai string
+		err = rows.Scan(&ID, &Nama, &SKS, &Jurusan, &SemesterMinimal, &PrediksiNilai)
+		if err != nil {
+			panic(err)
+		}
+		models.NewMataKuliah(ID, Nama, SKS, Jurusan, SemesterMinimal, PrediksiNilai)
+	}
+}
+
+// ReadAll reads all data from the database
+func ReadAll() {
+	ReadJurusan()
+	ReadMataKuliah()
+}
