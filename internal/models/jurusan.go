346f8af68b58a03a9a4d15bf344b2c399c6a6061
Marthenn <89301265+Marthenn@users.noreply.github.com>
2023-07-26 15:42:32 +0700

    feat: add jurusan set

--- internal/models/jurusan.go
+++ internal/models/jurusan.go
@@ -1,15 +1,21 @@
 package models
 
+import (
+	"github.com/golang-collections/collections/set"
+)
+
+var jurusanSet = set.New()
+
 type Jurusan struct {
 	Jurusan  string
 	Fakultas string
 }
 
-// NewJurusan creates a new jurusan object
-func NewJurusan(jurusan string, fakultas string) *Jurusan {
+// NewJurusan creates a new jurusan object and adds it to the set
+func NewJurusan(jurusan string, fakultas string) {
 	j := Jurusan{
 		Jurusan:  jurusan,
 		Fakultas: fakultas,
 	}
-	return &j
+	jurusanSet.Insert(j)
 }
