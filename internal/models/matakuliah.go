bb54e6936d4cdc82b98c34799870715e3889f30b
Marthenn <89301265+Marthenn@users.noreply.github.com>
2023-07-26 15:44:01 +0700

    feat: matakuliah class

---
+++ internal/models/matakuliah.go
@@ -0,0 +1,32 @@
+package models
+
+import (
+	"github.com/golang-collections/collections/set"
+)
+
+var MataKuliahSet = set.New()
+
+type MataKuliah struct {
+	ID              int
+	Nama            string
+	SKS             int
+	Jurusan         string
+	SemesterMinimal int
+	PrediksiNilai   string
+}
+
+// NewMataKuliah creates a new MataKuliah object
+func NewMataKuliah(ID int, Nama string, SKS int, Jurusan string, SemesterMinimal int, PrediksiNilai string) *MataKuliah {
+	// create the matakuliah object
+	mk := MataKuliah{
+		ID:              ID,
+		Nama:            Nama,
+		SKS:             SKS,
+		Jurusan:         Jurusan,
+		SemesterMinimal: SemesterMinimal,
+		PrediksiNilai:   PrediksiNilai,
+	}
+	MataKuliahSet.Insert(mk)
+
+	return &mk
+}
