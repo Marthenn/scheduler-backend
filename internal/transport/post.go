07f4cd5ca6ef0a5e16a0feeb20e13ba59a6b9c1c
Marthenn <89301265+Marthenn@users.noreply.github.com>
2023-07-30 21:41:52 +0700

    feat(REST): made the handler for new matkul and jurusan from json

--- internal/transport/post.go
+++ internal/transport/post.go
@@ -1,8 +1,11 @@
 package transport
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"scheduler-backend/internal/database"
+	"scheduler-backend/internal/models"
 )
 
 // postJurusan handles the POST request to /jurusan and adds a new jurusan
@@ -12,7 +15,19 @@ func postJurusan(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Println(r.Body)
+	type data struct {
+		Input []models.Jurusan `json:"Input"`
+	}
+	var jurusanJSON data
+	err := json.NewDecoder(r.Body).Decode(&jurusanJSON)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	for _, jurusan := range jurusanJSON.Input {
+		database.AddJurusan(jurusan.Jurusan, jurusan.Fakultas)
+	}
 }
 
 // postMataKuliah handles the POST request to /matakuliah and adds a new mata kuliah
@@ -22,7 +37,19 @@ func postMataKuliah(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Println(r.Body)
+	type data struct {
+		Input []models.MataKuliah `json:"Input"`
+	}
+	var mataKuliahJSON data
+	err := json.NewDecoder(r.Body).Decode(&mataKuliahJSON)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	for _, mataKuliah := range mataKuliahJSON.Input {
+		database.AddMatkul(mataKuliah.ID, mataKuliah.Nama, mataKuliah.SKS, mataKuliah.Jurusan, mataKuliah.SemesterMinimal, mataKuliah.PrediksiNilai)
+	}
 }
 
 // postResult handles the POST request to /find and returns the best matkul combination
